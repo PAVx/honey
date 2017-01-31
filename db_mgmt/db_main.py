49f50c084b84a413f1a8545e253601f539d600f9
harikuts <harikuttivelil@gmail.com>
2017-01-31 15:53:42 -0800

    basic database interaction files
    dbi - interface file (add stuff, remove stuff only interfacing file)
    db_main - core aggregation file, utilizes interface file
    db_conf - config file

---
+++ db_mgmt/db_main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+# filename: db_main.py
+# core database program
+
+import dbi
+from random import randint
+import ConfigParser
+import time
+
+# Configuration
+config_file_name = "dbi_conf"
+config = ConfigParser.ConfigParser()
+config.readfp(open(r'dbi_conf'))
+_core_variables = 'CORE_VARIABLES'
+# Set sample size
+n = int(config.get(_core_variables, 'n_samples'))
+num_instr = int(config.get(_core_variables, 'n_channels'))
+wait_time = 1/(float(config.get(_core_variables, 'sample_rate')))
+print "Sample size set to ", n, " every ", wait_time, " seconds." 
+
+
+# Instrument Channels
+class channel:
+    time = 0
+    x = 0
+    y = 0
+    z = 0
+    meas_val = [0] * n
+    pts = [0] * n
+
+channels = [channel for count in range(num_instr)]
+
+# Data Construction Functions
+def assemble_ch_data(ch, i):
+    channels[ch].meas_val[i] = randint(0,100)
+    channels[ch].pts[i] = randint(0,100)
+    channels[ch].time = randint(0,100)
+    channels[ch].x = randint(0,100)
+    channels[ch].y = randint(0,100)
+    channels[ch].z = randint(0,100)
+
+def n_average(lst):
+    total = 0
+    for val in lst:
+        total += val
+    return (total/len(lst))
+        
+        
+
+m = 0
+
+
+
+while(1):
+    assemble_ch_data(0, m)
+    
+    print(channels[0].meas_val[m])
+
+    # Check for nth measurement and store to database.
+    m = m + 1
+    if m >= n:
+        m = 0
+        # Add function: add_entry(inst_t, x, y, z, meas_val, pts)
+        dbi.add_entry("Instrument0_data",\
+                      channels[0].x,\
+                      channels[0].y,\
+                      channels[0].z,\
+                      n_average(channels[0].meas_val),\
+                      n_average(channels[0].pts))        
+
+    # Wait for tick
+    time.sleep(wait_time)
+
+dbi.fetch_all_entries("Instrument0_data")
+dbi._exit()
---
+++ db_mgmt/dbi.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+# filename: dbi.py
+# simple script testing out our wrapper program
+
+import MySQLdb
+import time
+import datetime
+from random import randint
+import ConfigParser
+
+# Get config file
+#print("Configuring interface program... ")
+config_file_name = "dbi_conf"
+config = ConfigParser.ConfigParser()
+config.readfp(open(r'dbi_conf'))
+_db_info = 'DB_MASTER_INFO'
+db_host = config.get(_db_info, 'host')
+db_user = config.get(_db_info, 'user')
+db_pass = config.get(_db_info, 'pass')
+db_inst = config.get(_db_info, 'dbin')
+#print("Configuration complete!\n")
+
+# Initialize the database object and cursor
+db = MySQLdb.connect(host=db_host, user=db_user, passwd=db_pass, db=db_inst)
+cur = db.cursor()
+
+# Add entry function
+def add_entry(inst_t, x, y, z, meas_val, pts):
+        #print("Adding entry... ")
+	# Get time
+	ts = time.time()
+	timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+	# Build query
+	add_data = (
+		"INSERT INTO `" + inst_t + "`(`time`, `loc_x`, `loc_y`, `loc_z`, `meas_val`, `pts`)"
+		"VALUES (%s, %s, %s, %s, %s, %s)"
+	)
+	#insert data
+	value_data = (timestamp, x, y, z, meas_val, pts)
+	#print(value_data)	
+	cur.execute(add_data, value_data)
+	db.commit()
+	print("Entry added.\n")
+
+def fetch_last_entry(table):
+        print ("Most recent entry:\n    ")
+        fetch_cmd = ("SELECT `time`, `loc_x`, `loc_y`, `loc_z`, `meas_val`, `pts` FROM `Instrument0_data` ORDER BY time DESC LIMIT 1")
+        cur.execute(fetch_cmd)
+        _print(cur)
+
+def fetch_all_entries(table):
+        print ("List all entries:\n    ")
+        fetch_cmd = ("SELECT * FROM `Instrument0_data` ORDER BY time")
+        cur.execute(fetch_cmd)
+        _print(cur)
+
+def manual_mysql(command):
+        cmd = command
+        cur.execute(cmd)
+        db.commit()
+        
+# DB close function
+def _exit():
+	cur.close()
+	db.close()
+
+
+def _print(result_cur):
+        result = cur.fetchall()
+        print_string = "|"
+        for row in result:
+                print_string = "| "
+                for col in row:
+                        this_string = str(col)
+                        that_string = " | "
+                        print_string = print_string  + this_string + that_string
+                print print_string
+
+# RUN PROGRAM
+##add_entry("Instrument0_data", randint(0,9), randint(0,9), randint(0,9), randint(0,9), randint(0,9))
+##fetch_all_entries("Instrument0_data")
+##_exit()

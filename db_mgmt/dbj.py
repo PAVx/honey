67f490c9ccc4c3ef7eca89acc330c475b0f84961
Nikola Panayotov <nikola.p.1337@gmail.com>
2017-04-04 02:34:48 +0000

    Improved dbj, added DB comm
    dbj.py now pulls the most recent values from the database and correctly writes them to a JSON file

--- db_mgmt/dbj.py
+++ db_mgmt/dbj.py
@@ -1,6 +1,8 @@
-# filename: json_gen2.py
-# Experimenting with json API
+# filename: dbj.py
+# Create data file to be visualized
 
+import MySQLdb
+import creds
 import os
 import shutil
 import ConfigParser
@@ -13,15 +15,21 @@ import time
 #Uncomment when integrated to Pi3#
 ##################################
 #Determine the number of sensors
-#config_file_name = "dbi_conf"
-#config = ConfigParser.ConfigParser()
-#config.readfp(open(r'dbi_conf'))
-#_core_variables = 'CORE_VARIABLES'
-#num_sensors = int(config.get(_core_variables, 'n_channels'))
+config_file_name = "dbi_conf"
+config = ConfigParser.ConfigParser()
+config.readfp(open(r'dbi_conf'))
+_db_info = 'DB_MASTER_INFO'
+_core_variables = 'CORE_VARIABLES'
+#Create database interfacing variables
+db_host = config.get(_db_info, 'host')
+db_user = config.get(_db_info, 'user')
+db_pass = creds.db_pass
+num_sensors = int(config.get(_core_variables, 'n_channels'))
 
 num_sensors = 1
 max_entries = 20
-
+db = MySQLdb.connect(host=db_host, user=db_user, passwd=db_pass)
+cur = db.cursor()
 datafile = "JSONData.json"
 
 #Initialize the JSON file
@@ -49,6 +57,26 @@ def jfile_addentry(filename, data_packet):
     for i in range(num_sensors):
         data['sensor'+str(i)] = int(data_packet[i+2])
     #data['sensor0'] = 9
+        json_data = (json.dumps(data,
+                indent=4, separators=(',', ': ')))
+    #print json_data
+    lines = jfile_scan(filename)
+    jfile = open(filename, 'w')
+    jfile.writelines([item for item in lines])
+    #jfile.write("\n")
+    jfile.write(json_data)
+    jfile.write("\n ]\n]")
+    jfile.close()
+
+#Add data_packet to the end of the JSON file. Seems slow right now
+def jfile_addentry2(filename, data_packet, k):
+    data = {}
+    dlen = len(data_packet)
+    flen = dlen / 2
+    data['time'] = k
+    for i in range(num_sensors):
+        data['sensor'+str(i)] = int(data_packet[0:flen])
+        data['pts'+str(i)] = int(data_packet[flen:])
         json_data = (json.dumps(data,
                 indent=4, separators=(',', ': ')))
     #print json_data
@@ -104,11 +132,50 @@ def jfile_pop(filename):
         jfile.close()
         jfile = open(filename, 'w')
         jfile.writelines([item for item in new_list])
-        jfile.close()        
-	
+        jfile.close()
+
+#Obtain the last n number of entries for a sensor from the specified table and place them in the JSON file
+def json_pull(n, table, sensor_num, datafile):
+        # Point to correct database
+        db = MySQLdb.connect(host=db_host, user=db_user, passwd=db_pass, db="jays")
+        cur = db.cursor()
+        # Get the last n entries
+        fetch_cmd = ("SELECT `sensor"+str(sensor_num)+"_data`, `sensor"+str(sensor_num)+"_pts` FROM `jay"+str(table)+"` ORDER BY time ASC LIMIT " + str(n))
+        cur.execute(fetch_cmd)
+        # Obtain the dataset object
+        dataset = cur.fetchall()
+        # Print to ensure it is correct
+        _print(dataset)
+        k = 0
+        # Each row is a DB entry
+        for row in dataset:
+            row_str = ""
+            # Obtain each field value and add it to the packet string
+            for col in row:
+                field = str(col)
+                row_str += field
+            #Print the packet to debug
+            print(row_str)
+            jfile_addentry2(datafile, row_str, k)
+            k += 1
+            #Every entry except final one has a comma
+            if(k < 20):
+                jfile_addcomma(datafile)
+            else:
+                jfile_update(datafile)
+     
+
+def _print(result):
+        print_string = "|"
+        for row in result:
+                print_string = "| "
+                for col in row:
+                        this_string = str(col)
+                        that_string = " | "
+                        print_string = print_string  + this_string + that_string
+                print print_string	
 
 #if __name__ == '__main__':
-print "Here i come i am cinnamon"
 jfile_init()
 #jfile_addentry(datafile, packet)
 #jfile_addcomma(datafile)
@@ -116,38 +183,33 @@ max_len = 5 + (max_entries *(3+num_sensors)) + ((max_entries)-1)
 i = 0
 file_full = 0
 state = 0
-while(1):
-        try:
-                packet = "%02d" % (i) + str(randint(0,9)) \
-                         + str(randint(0,9)) + str(randint(0,9))
-                i += 1
-                if(file_len(datafile) >= max_len):
-                        file_full = 1
-                #If the file is not full, just add an entry
-                if(file_full == 0):
-                        jfile_addentry(datafile, packet)
-                        jfile_addcomma(datafile)
-                        
-                #If file is full, add entry, pop the oldest value, and format
-                else:
-                        state = 0
-                        jfile_pop(datafile)
-                        state = 1
-                        jfile_addcomma2(datafile)
-                        state = 2
-                        jfile_addentry(datafile, packet)
-                        #jfile_update(datafile)                       
-                        #jfile_update(datafile)
-                        #time.sleep(1)
-##                        jfile_pop(datafile)
+json_pull(20, 0, 0, datafile)
+##while(1):
+##        try:
+##                packet = "%02d" % (i) + str(randint(0,9)) \
+##                         + str(randint(0,9)) + str(randint(0,9))
+##                i += 1
+##                if(file_len(datafile) >= max_len):
+##                        file_full = 1
+##                #If the file is not full, just add an entry
+##                if(file_full == 0):
 ##                        jfile_addentry(datafile, packet)
-##                        jfile_update(datafile)
-                        #break;
-        except KeyboardInterrupt:
-                break
-                if(state == 1):
-                        jfile_addcomma2(datafile)
-                        jfile_addentry(datafile, packet)
-                if(state == 2):
-                        jfile_addentry(datafile, packet)
-        
+##                        jfile_addcomma(datafile)
+##                        
+##                #If file is full, add entry, pop the oldest value, and format
+##                else:
+##                        state = 0
+##                        jfile_pop(datafile)
+##                        state = 1
+##                        jfile_addcomma2(datafile)
+##                        state = 2
+##                        jfile_addentry(datafile, packet)
+##                        #jfile_update(datafile)
+##                        
+##        except KeyboardInterrupt:
+##                break
+##                if(state == 1):
+##                        jfile_addcomma2(datafile)
+##                        jfile_addentry(datafile, packet)
+##                if(state == 2):
+##                        jfile_addentry(datafile, packet)

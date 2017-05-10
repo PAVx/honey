8cb305b00d8454351274f33e55fe0adefb37bdd9
Hari Kuttivelil <harikuttivelil@gmail.com>
2017-05-10 00:00:09 +0000

    Implemented state machine to receive and process only status messages from the XBee

--- db_mgmt/new_db_mgmt/db_main.py
+++ db_mgmt/new_db_mgmt/db_main.py
@@ -2,6 +2,7 @@
 # filename: db_main.py
 # Core database program
 
+import MySQLdb
 import dbi
 import dbz
 from dbs import drone_entry
@@ -11,6 +12,7 @@ import time
 import RPi.GPIO as GPIO
 import os
 import shutil
+import creds
 
 # Configuration
 config_file_name = "dbi_conf"
@@ -18,11 +20,17 @@ config = ConfigParser.ConfigParser()
 config.readfp(open(r'dbi_conf'))
 _core_variables = 'CORE_VARIABLES'
 _identifiers = 'IDENTIFIERS'
+_db_info = 'DB_MASTER_INFO'
 
 # Set sample size
 n = int(config.get(_core_variables, 'n_samples'))
 wait_time = 1/(float(config.get(_core_variables, 'sample_rate')))
 
+# Load credential information
+db_host0 = config.get(_db_info, 'host0')
+db_host1 = config.get(_db_info, 'host1')
+db_user = config.get(_db_info, 'user')
+db_pass = creds.db_pass
 # Load identifiers for ground and drones
 ground_db = config.get(_identifiers, 'rover')
 drone_db = config.get(_identifiers, 'drone')
@@ -53,16 +61,38 @@ GPIO.setup(CLOCK_IN, GPIO.IN)
 # Create array of channels to contain data sent from a drone
 drone_data = [drone_entry for count in range(num_drones)]
 
+# Create queues for local-to-cloud transfer
+online_flag = True
+cloud_queue = []
+for drone in drones_ref:
+    entry_queue = []
+    cloud_queue.append(entry_queue)
+
 # Init poll function from dbz
 dbz.init_poll()
  
 # Data Construction Functions
 def assemble_sensor_data(d, i):
-    drone_data[d] = dbz.poll(i)
+    while(1):
+        the_packet = dbz.poll(i)
+        if the_packet:
+            if the_packet.status_flag:
+                drone_data[d] = the_packet.entry
+                break
 
 def assemble_channels(i):
     for k in range(num_drones):
         assemble_sensor_data(k, i)
+
+def fake_assemble_sensor_data(d, i):
+    drone_data[d].t = randint(1, 100)
+    drone_data[d].x = randint(1, 100)
+    drone_data[d].y = randint(1, 100)
+    drone_data[d].z = randint(1, 100)
+
+def fake_assemble_channels(i):
+    for k in range(num_drones):
+        fake_assemble_sensor_data(k, i)
 
 def n_average(lst):
     total = 0
@@ -77,9 +107,10 @@ m = 0
 
 
 while(1):
-    try: 
+#    try: 
         # Wait for the rising edge of a "clock tick"
         # GPIO.wait_for_edge(CLOCK_IN, GPIO.RISING)
+        #time.sleep(0.5)
         # Proceed assembling data in all channels
         assemble_channels(m)        
         print "."
@@ -94,8 +125,11 @@ while(1):
                 for i in range(len(sensors_ref)):
                     drone_data[j].s[i].avg_data = n_average(drone_data[j].s[i].data)
                     drone_data[j].s[i].pts = drone_data[j].s[i].avg_data / 2
+                # Add entry to the appropriate queue to be moved to the cloud later
+                cloud_queue[j].append(drone_data[j])
                 # Add function: add_entry(inst_t, x, y, z, meas_val, pts)
-                dbi.add_entry(drone_db_real,\
+                dbi.add_entry(db_host1,\
+                              drone_db_real,\
                               drone,\
                               drone_data[j].x,\
                               drone_data[j].y,\
@@ -107,25 +141,56 @@ while(1):
         # Wait for tick
         # time.sleep(wait_time)
 
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+    # Online data transfer
+        if online_flag:
+            try:
+            # Connect to the cloud database
+                db = MySQLdb.connect(host=db_host0, user=db_user, passwd=db_pass, db=ground_db)
+                cur = db.cursor()
+            except:
+                print "Could not store to the cloud. Check connection."
+                online_flag = False
+            #try:
+            #print cloud_queue
+            if(online_flag and cloud_queue):
+                for q in range(len(cloud_queue)):
+                    print "Current Queue Length: " + str(len(cloud_queue[q]))
+                    while(len(cloud_queue[q])):
+                        drone_queue = cloud_queue[q]
+                        print "From the cloud: " + str(drone_queue)
+                        queued_entry = drone_queue.pop(0)
+                        print "Popped entry: " + str(queued_entry)
+                        dbi.add_entry(db_host0,\
+                                      drone_db_real,\
+                                      drones_ref[q],\
+                                      queued_entry.x,\
+                                      queued_entry.y,\
+                                      queued_entry.z,\
+                                      queued_entry.s)
+                    print "Queue has been cleared!"
+            #except:
+                #print "Bruh you clearly did some wack ass shit and now it's not working you hoe"
+
+    # Process escape
+#    except KeyboardInterrupt:
+ #       GPIO.cleanup()
         #delete the entire folder if it exists from a previous run
-        if os.path.exists("data"):
-            shutil.rmtree("data")
+  #      if os.path.exists("data"):
+   #         shutil.rmtree("data")
         #make the data folder
-        dbi._make_folder("data")
-        for drone in drones_ref:
-            dbi._make_folder("data/" + drone)
+    #    dbi._make_folder("data")
+     #   for drone in drones_ref:
+      #      dbi._make_folder("data/" + drone)
             #make the drone folders here
-            print "\n", drone
-            log = open("data/" + "/" + drone + "_log.csv", 'w')
-            print("File opened. Writing to file...")
-            dbi.write_all_entries(drone_db_real, drone, log)
-            log.close()
+       #     print "\n", drone
+        #    log = open("data/" + "/" + drone + "_log.csv", 'w')
+         #   print("File opened. Writing to file...")
+          #  dbi.write_all_entries(drone_db_real, drone, log)
+           # log.close()
             #dbi.fetch_all_entries(drone, ch)
-            dbi._exit(drone)
-        print "\n ALL ENTRIES LISTED! Program closing."
-        break
+            #dbi._exit(drone)
+       # print "\n ALL ENTRIES LISTED! Program closing."
+       # break
 
 ##GPIO.cleanup()
 ##dbi.fetch_all_entries("Instrument0_data")
--- db_mgmt/new_db_mgmt/dbz.py
+++ db_mgmt/new_db_mgmt/dbz.py
@@ -9,6 +9,7 @@ import time
 import datetime
 import ConfigParser
 import a2d
+import struct
 
 #Determine number of instruments and thus the packet length
 config_file_name = "dbi_conf"
@@ -32,75 +33,92 @@ ser.flushOutput()
 # Construct the door
 global the_door
 
+class packet():
+    status_flag = 0
+    src = 0
+    dest = 0
+    entry = drone_entry()
+
 def init_poll():
     # Construct the door
-    the_door = drone_entry()
+    the_door = packet()
     
 # Reads in data packet from serial port, parses retrieved packet
 def poll(i):
-    data_rec = 0;
+    data_rec = False
     try:
         data = ser.read(38)
-        #print "HEY LOOK AT ME: " + str(a2d.convert(data))
-        #print(data)
-        #print("Binary encoded:")
-        #bata = a2d.convert(data)
-        #print(bata)
-        data_rec = 1;
+        data_rec = True
     except:
         pass
     finally:
-        if(data_rec == 1):
+        if data_rec:
             data_bytes = bytes(data)
-            print(data_bytes)
+            #print(data_bytes)
             byte_array = a2d.b_array(data_bytes)
-            a2d.print_array(byte_array)
-            if(check_valid(byte_array) == 1):
+            #a2d.print_array(byte_array)
+            if check_valid(byte_array):
                 print("Valid start")
-                
-            #the_door = parse(data, i)
-            #return the_door
+                the_door = parse(byte_array, i)
+                return the_door
+            else:
+                return False
         else:
             print("No packet received")
 
 #Checks if status packet is received. Success = 1, Failure = 0
 def check_valid(packet_start):
     if(packet_start[0] != 1):
-        print("Invalid STX")
-        return 0
-    if(packet_start[1] != 2):
         print("Invalid SOH")
-        return 0
+        return False
+    if(packet_start[1] != 2):
+        print("Invalid STX")
+        return False
     if(packet_start[2] != 1):
-        print("Invalid Opcode")
-        return 0
+        print("Invalid Status Packet")
+        return False
     if(packet_start[4] != 170):
         print("Invalid Dest")
-        return 0
-    return 1
+        return False
+    return True
 
         
 # Obtains location, time, and sensor readings from received packet
 def parse(data, i):
-    ch = drone_entry()
-    ch.x = a2d.convert(data[6:13])
-    print "x=" + str(ch.x)
-    ch.y = a2d.convert(data[14:21])
-    print "y=" + str(ch.y)
-    ch.z = 0
-    ch.time = (data[22:29])
-    ch.s[0].data[i] = a2d.convert(data[30:37])
-    return ch
+    # Construct packet
+    pac = packet()
+    # Set flag to be true automatically as this is called after a check_valid
+    pac.status_flag = True
+    # Populate src and dest
+    pac.src = (data[4])
+    pac.dest = (data[5])
+    # Parse data
+    pac.entry.x = parse_loc(data[5:13])
+    print "x=" + str(pac.entry.x)
+    pac.entry.y = parse_loc(data[13:21])
+    print "y=" + str(pac.entry.y)
+    pac.entry.z = 0
+    pac.entry.time = parse_loc(data[21:29])
+    pac.entry.s[0].data[i] = parse_loc(data[29:37])
+    print "data for ", i, ": ", pac.entry.s[0].data[i]
+    # Return packet
+    return pac
 
 def parse_loc(byte_array):
+    print "byte_array: ", byte_array
     hex_string = ""
-    for i in range(8):
+    for i in range(len(byte_array)):
+        print "iter: ", i
         hex_val = hex(byte_array[i])
         hex_string += hex_val[2:].zfill(2)
     print("0x"+hex_string)
     parsed = struct.unpack('!d', hex_string.decode('hex'))[0]
-    print(parsed)
+    return parsed
 
+# MAIN FUNCTION: takes in sample iterationreturns false OR the packet class
+def get_packet(i):
+    return poll(i)
+    
 # Example code used to create functions in this file. Not relevant anymore.
 def main():
 
